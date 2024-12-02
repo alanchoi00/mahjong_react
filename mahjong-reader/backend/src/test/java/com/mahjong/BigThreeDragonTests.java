c87abd4c1ea187d93877120a1cfe3512f6e4c126
Alan Choi <115921805+alanchoi00@users.noreply.github.com>
2024-12-02 13:16:27 +1100

    tests: add BigThreeDragonTests (#13)

---
+++ mahjong-reader/backend/src/test/java/com/mahjong/BigThreeDragonTests.java
@@ -0,0 +1,106 @@
+package com.mahjong;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mahjong.controller.FaanCalculator;
+import com.mahjong.dto.MahjongRequest;
+import com.mahjong.dto.MahjongResponse;
+import com.mahjong.model.FaanType;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.InputStream;
+
+public class BigThreeDragonTests {
+
+  @Test
+  public void testBigFourWindWinVariation1() throws Exception {
+    // Create a sample request
+    String configFile = "./configs/big_three_dragon_win_v1.json";
+    ObjectMapper mapper = new ObjectMapper();
+    InputStream is = this.getClass().getResourceAsStream(configFile);
+    assertNotNull(is, "The configuration file should exist");
+    MahjongRequest requestObj = mapper.readValue(is, MahjongRequest.class);
+
+    // Simulate the calculate logic
+    FaanCalculator calculator = new FaanCalculator();
+    MahjongResponse response = calculator.calculate(requestObj);
+
+    // Validate the response
+    assertEquals(response.getFaan(), 8);
+    assertTrue(response.hasFaanType(FaanType.BIG_THREE_DRAGONS));
+  }
+
+  @Test
+  public void testBigFourWindWinVariation2() throws Exception {
+    // Create a sample request
+    String configFile = "./configs/big_three_dragon_win_v2.json";
+    ObjectMapper mapper = new ObjectMapper();
+    InputStream is = this.getClass().getResourceAsStream(configFile);
+    assertNotNull(is, "The configuration file should exist");
+    MahjongRequest requestObj = mapper.readValue(is, MahjongRequest.class);
+
+    // Simulate the calculate logic
+    FaanCalculator calculator = new FaanCalculator();
+    MahjongResponse response = calculator.calculate(requestObj);
+
+    // Validate the response
+    assertEquals(response.getFaan(), 8);
+    assertTrue(response.hasFaanType(FaanType.BIG_THREE_DRAGONS));
+  }
+
+  @Test
+  public void testBigFourWindWinVariation3() throws Exception {
+    // Create a sample request
+    String configFile = "./configs/big_three_dragon_win_v3.json";
+    ObjectMapper mapper = new ObjectMapper();
+    InputStream is = this.getClass().getResourceAsStream(configFile);
+    assertNotNull(is, "The configuration file should exist");
+    MahjongRequest requestObj = mapper.readValue(is, MahjongRequest.class);
+
+    // Simulate the calculate logic
+    FaanCalculator calculator = new FaanCalculator();
+    MahjongResponse response = calculator.calculate(requestObj);
+
+    // Validate the response
+    assertEquals(response.getFaan(), 8);
+    assertTrue(response.hasFaanType(FaanType.BIG_THREE_DRAGONS));
+  }
+
+  @Test
+  public void testBigFourWindFailNoPairVariation1() throws Exception {
+    // Create a sample request
+    String configFile = "./configs/big_three_dragon_fail_noPair_v1.json";
+    ObjectMapper mapper = new ObjectMapper();
+    InputStream is = this.getClass().getResourceAsStream(configFile);
+    assertNotNull(is, "The configuration file should exist");
+    MahjongRequest requestObj = mapper.readValue(is, MahjongRequest.class);
+
+    // Simulate the calculate logic
+    FaanCalculator calculator = new FaanCalculator();
+    MahjongResponse response = calculator.calculate(requestObj);
+
+    // Validate the response
+    assertEquals(response.getFaan(), 0);
+    assertTrue(response.hasFaanType(FaanType.NO_FAAN));
+  }
+
+  @Test
+  public void testBigFourWindFailNoPairVariation2() throws Exception {
+    // Create a sample request
+    String configFile = "./configs/big_three_dragon_fail_noPair_v2.json";
+    ObjectMapper mapper = new ObjectMapper();
+    InputStream is = this.getClass().getResourceAsStream(configFile);
+    assertNotNull(is, "The configuration file should exist");
+    MahjongRequest requestObj = mapper.readValue(is, MahjongRequest.class);
+
+    // Simulate the calculate logic
+    FaanCalculator calculator = new FaanCalculator();
+    MahjongResponse response = calculator.calculate(requestObj);
+
+    // Validate the response
+    assertEquals(response.getFaan(), 0);
+    assertTrue(response.hasFaanType(FaanType.NO_FAAN));
+  }
+
+}
